a6a625e4d679d7089e612b0fcd150126ee083c7d
FroggyKwa <ilya.laguxa@gmail.com>
2022-07-11 14:50:53 +1000

    fix ssl

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from database.db import engine
 from models import models, schemas
